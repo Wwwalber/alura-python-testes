bdac514a3d8117f9e7d067d795ad0a72be78c713
Walber Costa <walberoc@gmail.com>
2026-09-21 13:34:28 -0300

    feat: criação de testes unitários para o modelo de usuários utilizando pytest

--- burguer-app/auth-service/test/test_user_model.py
+++ burguer-app/auth-service/test/test_user_model.py
@@ -23,3 +23,71 @@ def test_serialize_user_completo():
     }
     assert resultado == esperado
 
+def test_serialize_user_incompleto():
+    user = {
+        "email": "teste@exemplo.com",
+    }
+    resultado = serialize_user(user)
+    esperado = {
+        "email": "teste@exemplo.com",
+        "name": "",
+        "address": "",
+        "role": "cliente"
+    }
+    assert resultado == esperado
+
+def test_serialize_user_vazio():
+    user = {}
+    resultado = serialize_user(user)
+    esperado = {
+        "email": None,
+        "name": "",
+        "address": "",
+        "role": "cliente"
+    }
+    assert resultado == esperado
+
+def test_serialize_user_inteiro():
+    with pytest.raises(AttributeError):
+        serialize_user(12345)
+
+def test_serialize_user_lista():
+    with pytest.raises(AttributeError):
+        serialize_user("String de teste")        
+
+def test_serialize_user_none():
+    with pytest.raises(AttributeError):
+        serialize_user(None)
+
+def test_serialize_user_inesperado():
+    user = {
+        "email": "123456",
+        "name": ["nome","sobrenome"],
+        "address":{"rua": "Rua de exemplo"},
+        "role": True
+    } 
+    resultado = serialize_user(user) 
+    esperado = {  
+        "email": "123456",
+        "name": ["nome","sobrenome"], 
+        "address":{"rua": "Rua de exemplo"},
+        "role": True
+    }
+    assert resultado == esperado
+
+def test_serialize_user_dict_none():
+    user = {
+        "email": None,
+        "name": None,
+        "address": None,
+        "role": None
+    }
+    resultado = serialize_user(user)
+    esperado = {
+        "email": None,
+        "name": None,
+        "address": None,
+        "role": None
+    }
+    assert resultado == esperado
+
